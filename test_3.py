d82990fa6699b482c124d962849a66bac584de49
David <davidbutaev123321@gmail.com>
2021-03-26 19:45:56 +0300

    Com

---
+++ test_3.py
@@ -0,0 +1,13 @@
+def appearance(intervals):
+    time = []
+    for k in range(0, len(intervals['pupil']), 2):
+        for j in range(0, len(intervals['tutor']), 2):
+            tests = {'lesson': intervals['lesson'],
+                     'pupil': intervals['pupil'][k:k + 2],
+                     'tutor': intervals['tutor'][j:j + 2]}
+            les = range(tests['lesson'][0], tests['lesson'][1] + 1)
+            pup = range(tests['pupil'][0], tests['pupil'][1] + 1)
+            tut = range(tests['tutor'][0], tests['tutor'][1] + 1)
+            alls = set(les).intersection(pup).intersection(tut)
+            time.append(max(alls, default=0) - min(alls, default=0))
+    return sum(time)
